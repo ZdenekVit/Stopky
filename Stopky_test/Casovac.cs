2dba648905c2db930bf451e4568ebd1fcf53edbb
Zdeněk Vitner <zdenek.vitner@spsrakovnik.onmicrosoft.com>
2025-08-19 18:01:32 +0200

    Pridani cislovani kol,vypocet mezicasu, optimalizovaní formatovani a pridana metoda na formatovani

--- Stopky_test/Casovac.cs
+++ Stopky_test/Casovac.cs
@@ -11,12 +11,25 @@ namespace Stopky_test
     {
         DateTime zacatek;
         TimeSpan konec;
+        TimeSpan nula = DateTime.Now - DateTime.Now; 
 
-        List<string> Zaznami = new List<string>();
-
+        //list docasne drzíci zaznami
+        List<TimeSpan> Zaznami = new List<TimeSpan>();
+        //list docasne drzíci zaznami mezicasu
+        List<TimeSpan> ZaznamiMeziCasu = new List<TimeSpan>();
         //Konstruktor
         public Casovac() 
         {
+            //vlozí původní záznam 00:00:00:00 aby první mezicas vysel
+            ZaznamiMeziCasu.Add(nula);
+        }
+
+        //Preformatuje datovy typ TimeSpan na String a zkrati presnost milisekund na 2 desetina cisla
+        public string Formatovat(TimeSpan formatovat)
+        {
+            string vysledek = string.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                (int)formatovat.TotalHours, formatovat.Minutes, formatovat.Seconds, formatovat.Milliseconds / 10);
+            return vysledek;
         }
 
         //Spustí se casomira
@@ -31,11 +44,8 @@ namespace Stopky_test
                 Console.SetCursorPosition(0, 0);
                 //Získá aktualni cas
                 konec = (DateTime.Now - zacatek);
-                //Preformatuje datovy typ TimeSpan na String a zkrati presnost milisekund na 2 desetina cisla
-                string aktualniCas = string.Format("{0:00}:{1:00}:{2:00}:{3:00}",
-                (int)konec.TotalHours, konec.Minutes, konec.Seconds, konec.Milliseconds / 10);
                 //Vypise cas
-                Console.Write($"\r   {aktualniCas}   ");
+                Console.Write($"\r   {Formatovat(konec)}   ");
 
                 //Ceka na stisknuti klavesy
                 if (Console.KeyAvailable)
@@ -45,17 +55,19 @@ namespace Stopky_test
                     //pokud je klavesa K
                     if (volba.Key == ConsoleKey.K)
                     {
-                        Kolo(aktualniCas);
+                        Kolo(konec,konec);
                     }
                 }
             }
         }
 
         //udelá zaznam o kole + pridá záznam do historie a vypíše max 5 zaznamu
-        public void Kolo(string aktualniCas)
+        public void Kolo(TimeSpan aktualniCas, TimeSpan meziCas)
         {
-            //prida zaznam do seznamu nedavnych zaznamu
+            //prida zaznam do seznamu zaznamu
             Zaznami.Add(aktualniCas);
+            //prida zaznam do seznamu mezicasu
+            ZaznamiMeziCasu.Add(meziCas);
             //nastavi startovni hodnotu kurzoru pro nedavnou historii
             int poziceKurzoruVHistorii = 2;
             //dynamicky projede seznam od zadu
@@ -66,13 +78,14 @@ namespace Stopky_test
                 //pridava zaznami odshora 
                 if (poziceKurzoruVHistorii <= 5)
                 {
-                    Console.WriteLine(Zaznami[i]);
+                    Console.WriteLine((i + 1) + " --- " + Formatovat(ZaznamiMeziCasu[i+1] - ZaznamiMeziCasu[i]) + " --- " + Formatovat(Zaznami[i]));
                     poziceKurzoruVHistorii++;
+
                 }
                 else
                 {
                     //pokud pocet zaznamu presahne 5 ukonci projizdeni seznamu
-                    Console.WriteLine(Zaznami[i]);
+                    Console.WriteLine((i + 1) + " --- " + Formatovat(ZaznamiMeziCasu[i + 1] - ZaznamiMeziCasu[i]) + " --- " + Formatovat(Zaznami[i]));
                     poziceKurzoruVHistorii = 2;
                     i = 0;
                 }
