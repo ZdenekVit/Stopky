2300bc902980dab12b97228420236ec0a64ddb0c
Zdeněk Vitner <zdenek.vitner@spsrakovnik.onmicrosoft.com>
2025-08-19 17:01:26 +0200

    Vytvoreni metody Kolo a list zaznamu

--- Stopky_test/Casovac.cs
+++ Stopky_test/Casovac.cs
@@ -11,6 +11,9 @@ namespace Stopky_test
     {
         DateTime zacatek;
         TimeSpan konec;
+
+        List<string> Zaznami = new List<string>();
+
         //Konstruktor
         public Casovac() 
         {
@@ -33,6 +36,46 @@ namespace Stopky_test
                 (int)konec.TotalHours, konec.Minutes, konec.Seconds, konec.Milliseconds / 10);
                 //Vypise cas
                 Console.Write($"\r   {aktualniCas}   ");
+
+                //Ceka na stisknuti klavesy
+                if (Console.KeyAvailable)
+                {
+                    //nacte stisknutou klavesu
+                    ConsoleKeyInfo volba = Console.ReadKey();
+                    //pokud je klavesa K
+                    if (volba.Key == ConsoleKey.K)
+                    {
+                        Kolo(aktualniCas);
+                    }
+                }
+            }
+        }
+
+        //udelá zaznam o kole + pridá záznam do historie a vypíše max 5 zaznamu
+        public void Kolo(string aktualniCas)
+        {
+            //prida zaznam do seznamu nedavnych zaznamu
+            Zaznami.Add(aktualniCas);
+            //nastavi startovni hodnotu kurzoru pro nedavnou historii
+            int poziceKurzoruVHistorii = 2;
+            //dynamicky projede seznam od zadu
+            for (int i = Zaznami.Count - 1; i >= 0; i--)
+            {
+                //nastavi startovni pozici kurzoru
+                Console.SetCursorPosition(0, poziceKurzoruVHistorii);
+                //pridava zaznami odshora 
+                if (poziceKurzoruVHistorii <= 5)
+                {
+                    Console.WriteLine(Zaznami[i]);
+                    poziceKurzoruVHistorii++;
+                }
+                else
+                {
+                    //pokud pocet zaznamu presahne 5 ukonci projizdeni seznamu
+                    Console.WriteLine(Zaznami[i]);
+                    poziceKurzoruVHistorii = 2;
+                    i = 0;
+                }
             }
         }
 
