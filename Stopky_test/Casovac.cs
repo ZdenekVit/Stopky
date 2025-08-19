e817747c718b29da510ab0144932a179d6c26cdd
Zdeněk Vitner <zdenek.vitner@spsrakovnik.onmicrosoft.com>
2025-08-19 15:51:58 +0200

    Zalozeni trid

---
+++ Stopky_test/Casovac.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopky_test
+{
+    internal class Casovac
+    {
+
+    }
+}
